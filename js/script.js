c4a054b9b66871c268f91b561efb21f0c4653c29
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-16 18:19:02 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
-// const bici = [
+const bici = [
 
-//     {
-//         nome : 'Pinarello',
-//         peso : 10,
-//     },
+    {
+        nome : 'Pinarello',
+        peso : 10,
+    },
 
-//     {
-//         nome : 'Cross',
-//         peso : 5,
-//     },
+    {
+        nome : 'Cross',
+        peso : 5,
+    },
 
-//     {
-//         nome : 'Mountain',
-//         peso : 3,
-//     }
-// ]
+    {
+        nome : 'Mountain',
+        peso : 3,
+    }
+]
 
-// let {nome,peso} = bici[0];
+let {nome,peso} = bici[0];
 
-// for (var i = 0; i < bici.length; i++) {
+for (var i = 0; i < bici.length; i++) {
 
-//     if (bici[i].peso < peso) {
-//         peso = bici[i].peso;
-//         nome = bici[i].nome;
-//     }
+    if (bici[i].peso < peso) {
+        peso = bici[i].peso;
+        nome = bici[i].nome;
+    }
     
-// }
+}
 
-// console.log(`La bici ${nome} é la piú leggera con soli ${peso}kg`);
+console.log(`La bici ${nome} é la piú leggera con soli ${peso}kg`);
 
 
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (Utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti. Stampiamo tutto in console.
@@ -61,7 +61,7 @@ for (let i = 0; i < squadre.length; i++) {
     squadre[i].puntiFatti, squadre[i].puntiSubiti = random();
 
     let {nome,puntiSubiti} = squadre[i];
-    
+
     newSquadre.push({nome : nome, puntiSubiti : puntiSubiti});
     
 };
